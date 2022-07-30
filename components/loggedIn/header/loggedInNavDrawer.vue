ac4607d75abc4dfd5af06f51a6be22fe90509fb9
kuu18 <kdai18.kuu@gmail.com>
2022-07-30 16:36:40 +0900

    ナビゲーションドロワーの追加

---
+++ components/loggedIn/header/loggedInNavDrawer.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-navigation-drawer
+      v-model="syncedDrawer"
+      app
+      clipped
+      mobile-breakpoint="960"
+    >
+      <v-sheet
+        color="grey lighten-4"
+        class="pa-4"
+      >
+        <v-avatar
+          class="mb-4"
+          color="grey darken-1"
+          size="64"
+        >
+          <img
+            alt="Avatar"
+            :src="$auth.currentUser.avatarUrl"
+          >
+        </v-avatar>
+
+        <div>{{ $auth.currentUser.username }}</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+          v-for="[icon, text] in links"
+          :key="icon"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <template v-slot:append>
+        <div class="pa-2 mb-10">
+          <v-btn block>
+            投稿する
+          </v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
+
+</template>
+
+<script lang='ts'>
+import { Vue, Component, PropSync } from 'nuxt-property-decorator';
+
+@Component
+export default class LoggedInNavDrawer extends Vue {
+  links= [
+    ['mdi-inbox-arrow-down', 'Inbox'],
+    ['mdi-send', 'Send'],
+    ['mdi-delete', 'Trash'],
+    ['mdi-alert-octagon', 'Spam'],
+  ]
+
+  @PropSync('drawer', { type: Boolean, default: null })
+    syncedDrawer!: boolean
+
+}
+</script>
---
+++ components/loggedIn/loggedInContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container fluid>
+    <slot name="my-content" />
+  </v-container>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class LoggedInContainer extends Vue {
+}
+</script>
